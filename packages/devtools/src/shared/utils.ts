d6f0366a4c6eba14c0bd8b1259f70f8b9a44a7af
Anthony Fu <github@antfu.me>
2025-04-10 17:31:37 +0900

    build: use `rolldown-vite`

--- packages/devtools/src/shared/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { NpmMetaLatest } from 'node-modules-tools'
-
-export function isNpmMetaLatestValid(meta?: NpmMetaLatest): boolean {
-  if (!meta)
-    return false
-  if (meta.vaildUntil < Date.now())
-    return false
-  return !!meta.publishedAt
-}
