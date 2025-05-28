fc7ab65bf22e3741f103b2cb6360e1be14ae1e79
Anthony Fu <github@antfu.me>
2025-05-28 14:52:17 +0900

    chore: use tsdown and clean up deps

--- packages/devtools/tsdown.config.ts
+++ packages/devtools/tsdown.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: [
+    'src/node/cli.ts', // Workaround for empty chunk
     'src/index.ts',
     'src/cli.ts',
     'src/dirs.ts',
   ],
   clean: false,
   target: 'esnext',
+  fixedExtension: true,
 })
