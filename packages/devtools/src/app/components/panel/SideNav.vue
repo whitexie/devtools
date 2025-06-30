5942eeff257a4348ceb10b3ef3f7480c9b69e1b7
Anthony Fu <github@antfu.me>
2025-06-30 13:50:32 +0900

    feat: side nav ui

--- packages/devtools/src/app/components/panel/SideNav.vue
+++ packages/devtools/src/app/components/panel/SideNav.vue
@@ -15,21 +15,26 @@ const items = computed<SideNavItem[]>(() => [
 </script>
 
 <template>
-  <div border="r base" flex="~ col gap-1" p1 of-y-scroll bg-base relative>
-    <template v-for="item in items" :key="item.title">
-      <component
-        :is="item.to ? NuxtLink : 'button'"
-        v-bind="item.to ? { to: item.to } : {}"
-        v-tooltip="item.title"
-        :title="item.title"
-        rounded-full
-        p2 hover:bg-active op-fade hover:op100
-        flex="~ items-center justify-center"
-        exact-active-class="text-primary op100!"
-        @click="item.action?.()"
-      >
-        <div :class="item.icon" text-lg />
-      </component>
-    </template>
+  <div>
+    <div
+      border="r y base rounded-r-xl" flex="~ col gap-1" p1 of-y-scroll max-h-96vh relative bg-glass
+      class="fixed left-0 top-1/2 -translate-y-1/2"
+    >
+      <template v-for="item in items" :key="item.title">
+        <component
+          :is="item.to ? NuxtLink : 'button'"
+          v-bind="item.to ? { to: item.to } : {}"
+          v-tooltip="{ placement: 'right', content: item.title }"
+          :title="item.title"
+          rounded-full
+          p2 hover:bg-active op-fade hover:op100
+          flex="~ items-center justify-center"
+          exact-active-class="text-primary op100!"
+          @click="item.action?.()"
+        >
+          <div :class="item.icon" text-lg />
+        </component>
+      </template>
+    </div>
   </div>
 </template>
--- packages/devtools/src/app/pages/session/[session]/plugins.vue
+++ packages/devtools/src/app/pages/session/[session]/plugins.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+import type { SessionContext } from '~~/shared/types'
+
 defineProps<{
-  modelValue?: boolean
+  session: SessionContext
 }>()
 </script>
 
 <template>
-  <div>
-    Plugins
+  <div p4>
+    <div py2 op50>
+      Plugins ({{ session.meta.plugins.length }})
+    </div>
+
+    <div flex="~ col gap-1">
+      <template v-for="plugin in session.meta.plugins" :key="plugin.id">
+        <div font-mono>
+          <DisplayPluginName :name="plugin.name" />
+        </div>
+      </template>
+    </div>
   </div>
 </template>
