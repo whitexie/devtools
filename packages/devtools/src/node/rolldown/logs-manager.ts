81d90f40653d3e54a50bea87f22918a92de81ec5
青椒肉丝 <58922004+pkc918@users.noreply.github.com>
2025-07-14 14:06:22 +0800

    fix: support JSON format for meta.json file (#15)

--- packages/devtools/src/node/rolldown/logs-manager.ts
+++ packages/devtools/src/node/rolldown/logs-manager.ts
@@ -22,7 +22,9 @@ export class RolldownLogsManager {
     return await Promise.all(sessions
       .filter(d => d.isDirectory())
       .map(async (d): Promise<BuildInfo> => {
-        const meta = JSON.parse(await fs.readFile(join(this.dir, d.name, 'meta.json'), 'utf-8')) as SessionMeta
+        const reader = RolldownEventsReader.get(join(this.dir, d.name, 'meta.json'))
+        await reader.read()
+        const meta = reader.manager.events[reader.manager.events.length - 1] as SessionMeta
         return {
           id: d.name,
           // @ts-expect-error missing type
@@ -35,7 +37,8 @@ export class RolldownLogsManager {
 
   async loadSession(session: string) {
     const reader = RolldownEventsReader.get(join(this.dir, session, 'logs.json'))
-    reader.meta ||= JSON.parse(await fs.readFile(join(this.dir, session, 'meta.json'), 'utf-8')) as SessionMeta
+    const metaReader = RolldownEventsReader.get(join(this.dir, session, 'meta.json'))
+    reader.meta = metaReader.manager.events[metaReader.manager.events.length - 1] as SessionMeta
     await reader.read()
     return reader
   }
