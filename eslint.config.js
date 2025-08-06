dad7d6c3f1815da3d36104b24d17a9a647e8968c
Anthony Fu <github@antfu.me>
2025-08-06 15:23:34 +0900

    refactor: rename `@vitejs/devtools` to `@vitejs/devtools-vite`  package (#68)

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
-import nuxt from './packages/devtools/src/.nuxt/eslint.config.mjs'
+import nuxt from './packages/devtools-vite/src/.nuxt/eslint.config.mjs'
 
 export default antfu({
   pnpm: true,
 })
   .append(nuxt())
   .append({
-    files: ['packages/devtools/src/node/**/*.ts'],
+    files: ['packages/devtools-vite/src/node/**/*.ts'],
     rules: {
       'no-console': 'off',
     },
