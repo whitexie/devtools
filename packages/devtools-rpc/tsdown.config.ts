622480fcccb717fcdd18bb270fc2f2e8672d8eb5
Anthony Fu <github@antfu.me>
2025-08-06 15:32:59 +0900

    chore: update tsconfig

--- packages/devtools-rpc/tsdown.config.ts
+++ packages/devtools-rpc/tsdown.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig([{
-  entry: [
-    'src/index.ts',
-  ],
-  clean: true,
-  fixedExtension: true,
-  dts: { isolatedDeclarations: true },
-}, {
-  entry: ['src/presets/ws/client.ts', 'src/presets/ws/server.ts', 'src/presets/index.ts'],
-  clean: true,
-  fixedExtension: true,
-  outDir: 'dist/presets',
-  dts: { isolatedDeclarations: true },
-}])
+export default defineConfig([
+  {
+    entry: [
+      'src/index.ts',
+    ],
+    clean: true,
+    fixedExtension: true,
+    dts: true,
+  },
+  {
+    entry: ['src/presets/ws/client.ts', 'src/presets/ws/server.ts', 'src/presets/index.ts'],
+    clean: true,
+    fixedExtension: true,
+    outDir: 'dist/presets',
+    dts: true,
+  },
+])
--- packages/devtools-vite/tsdown.config.ts
+++ packages/devtools-vite/tsdown.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
   clean: false,
   target: 'esnext',
   fixedExtension: true,
-  dts: { isolatedDeclarations: true },
+  dts: true,
 })
